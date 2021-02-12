100fadeece479a6c805f1590b1664b2db7be55cd
Karunakar.T <karunakarthurlujava@gmail.com>
2021-02-12 08:38:27 +0530

    adding git ignore

--- emitters/BasicEmitterEx.js
+++ emitters/BasicEmitterEx.js
@@ -5,3 +5,22 @@ evementEmitter.on('sum', (a, b) => {
     console.log("sum emmiter triggred :" + (a + b));
 });
 evementEmitter.emit('sum', 2, 8);
+
+class Persion extends EventEmitter {
+    constructor(name) {
+        super();
+        this._name = name;
+    }
+    get name() {
+        return this._name;
+    }
+}
+
+let person = new Persion("karunakar");
+
+person.on('name', () => {
+    console.log(`Hai....${person.name}`)
+})
+
+
+person.emit('name');
